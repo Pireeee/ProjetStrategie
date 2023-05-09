5b30ac2031ffd8d4f5e6501d8522ebde2af87cb1
Pire <p.clement01@orange.fr>
2023-05-09 16:30:49 +0200

    UniteSimpleGénérator

--- src/main/Outil.java
+++ src/main/Outil.java
@@ -3,4 +3,9 @@ package main;
 public class Outil {
     public int niveau;
     public TypeRessource type;
+
+    public Outil(TypeRessource type) {
+        this.type = type;
+        this.niveau = 1;
+    }
 }
--- src/main/unite/UniteSimple.java
+++ src/main/unite/UniteSimple.java
@@ -1,6 +1,7 @@
 package main.unite;
 
 import main.Outil;
+import main.TypeRessource;
 
 public class UniteSimple {
     public String nom;
@@ -8,4 +9,12 @@ public class UniteSimple {
     public boolean monte;
     public Outil outil;
     public int exp;
+
+    public UniteSimple(String nom, TypeRessource typeRessource) {
+        this.nom = nom;
+        this.outil = new Outil(typeRessource);
+        this.expert = false;
+        this.monte = false;
+        this.exp = 0;
+    }
 }
