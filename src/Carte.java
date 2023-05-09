a7b1d6991ad835b5ca5659a396dc4543f34365e9
Pire <p.clement01@orange.fr>
2023-05-09 15:45:09 +0200

    AfficherCarte

--- src/Carte.java
+++ src/Carte.java
@@ -8,4 +8,31 @@ public class Carte {
         this.hauteur = hauteur;
         this.cases = cases;
     }
+    public void afficher(){
+        for (int i = 0; i < this.hauteur; i++) {
+            for (int j = 0; j < this.largeur; j++) {
+                System.out.print("[");
+                if (this.cases[i][j].ressource == null)
+                    System.out.print("0");
+                else{
+                    switch (this.cases[i][j].ressource.type) {
+                        case NOURRITURE:
+                            System.out.print("N");
+                            break;
+                        caseBOIS:
+                            System.out.print("B");
+                            break;
+                        case OR:
+                            System.out.print("O");
+                            break;
+                        case PIERRE:
+                            System.out.print("P");
+                            break;
+                    }
+                }
+                System.out.println("]");
+            }
+            System.out.println("\n");
+        }
+    }
 }
