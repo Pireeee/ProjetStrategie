c1f8d14fa1bc8b4b9d65b44be86af2f09bae1324
Pire <p.clement01@orange.fr>
2023-05-09 14:45:49 +0200

    Carte

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
+    public static Carte carte;
     public static void main(String[] args) {
+        carte = new Carte(10, 10, new Case[10][10]);
+
         System.out.println("Hello world!");
     }
 }
